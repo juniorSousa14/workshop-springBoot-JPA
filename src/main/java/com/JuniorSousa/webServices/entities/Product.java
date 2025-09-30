3923a271a27027a380c6c9c44729608e420a82a0
Junior Sousa <jose.valberto@maisunifacisa.com.br>
2025-09-30 20:30:43 -0300

    Payment, one-to-one association

--- src/main/java/com/JuniorSousa/webServices/entities/Product.java
+++ src/main/java/com/JuniorSousa/webServices/entities/Product.java
@@ -1,5 +1,6 @@
 package com.JuniorSousa.webServices.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
@@ -20,6 +21,9 @@ public class Product implements Serializable {
     private String imgUrl;
 
     private Set<Category> categories = new HashSet<>();
+
+    @OneToMany(mappedBy = "id.product")
+    private Set<OrderItem> items = new HashSet<>();
 
     public Product() {
     }
@@ -76,6 +80,14 @@ public class Product implements Serializable {
         this.price = price;
     }
 
+    @JsonIgnore
+    public Set<Order> getOrders() {
+        Set<Order> set = new HashSet<>();
+        for (OrderItem x : items) {
+            set.add(x.getOrder());
+        }
+        return set;
+    }
     @Override
     public boolean equals(Object o) {
         if (o == null || getClass() != o.getClass()) return false;
