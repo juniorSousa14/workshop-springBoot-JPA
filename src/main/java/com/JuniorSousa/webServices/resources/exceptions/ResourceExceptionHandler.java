038c71d70c5e0c4936657ce60b1b5d165d4eea50
Junior Sousa <jose.valberto@maisunifacisa.com.br>
2025-09-30 21:34:38 -0300

    Exception handling - update

--- src/main/java/com/JuniorSousa/webServices/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/JuniorSousa/webServices/resources/exceptions/ResourceExceptionHandler.java
@@ -26,6 +26,4 @@ public class ResourceExceptionHandler {
         StandardError err = new StandardError(System.currentTimeMillis(), status.value(), error, e.getMessage(), request.getRequestURI());
         return ResponseEntity.status(status).body(err);
     }
-
-
 }
